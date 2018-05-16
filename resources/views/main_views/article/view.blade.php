9c04bbd28ee0692c20bed98cb883669f8ad8f85d
Jose Sicajan <sicajancoy@gmail.com>
2018-05-16 02:35:30 -0600

    add this for article

--- resources/views/main_views/article/view.blade.php
+++ resources/views/main_views/article/view.blade.php
@@ -57,4 +57,6 @@
         });
     });
 </script>
+<!-- Go to www.addthis.com/dashboard to customize your tools -->
+<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5afbebe2d2bf457e"></script>
 @endsection
