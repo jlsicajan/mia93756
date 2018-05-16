2c70f21311fa778aa048cc222a9640cb7f23c00e
Jose Sicajan <sicajancoy@gmail.com>
2018-05-16 01:42:48 -0600

    comment on the article page

--- resources/views/main_views/article/view.blade.php
+++ resources/views/main_views/article/view.blade.php
@@ -31,7 +31,7 @@
     </div>
     <div class="row">
         <div class="col-12">
-            <div class="fb-comments" data-href="https://developers.facebook.com/docs/plugins/comments#configurator" data-numposts="5"></div>
+            <div class="fb-comments" data-href="http://mia937.elcaminoweb.com" data-width="100%" data-numposts="5"></div>
         </div>  
     </div>
 
@@ -40,12 +40,13 @@
 
 @section('after_body')
 <div id="fb-root"></div>
+<div id="fb-root"></div>
 <script>
     (function(d, s, id) {
       var js, fjs = d.getElementsByTagName(s)[0];
       if (d.getElementById(id)) return;
       js = d.createElement(s); js.id = id;
-      js.src = 'https://connect.facebook.net/es_LA/sdk.js#xfbml=1&version=v3.0&appId=167238943956140&autoLogAppEvents=1';
+      js.src = 'https://connect.facebook.net/es_LA/sdk.js#xfbml=1&version=v3.0&appId=1765073390470250&autoLogAppEvents=1';
       fjs.parentNode.insertBefore(js, fjs);
   }(document, 'script', 'facebook-jssdk'));
 </script>
