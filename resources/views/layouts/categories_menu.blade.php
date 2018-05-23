aed34988ecf8966db54947109c8e4d5fb3bd96d8
Jose Sicajan <sicajancoy@gmail.com>
2018-05-22 21:24:49 -0600

    url dinamy with url field and css updated

--- resources/views/layouts/categories_menu.blade.php
+++ resources/views/layouts/categories_menu.blade.php
@@ -5,10 +5,17 @@
 @foreach(App\Category::list_for_menu() as $category)
     <li class="nav-item dropdown">
         @if(empty($category['subcategories']))
-            <a class="nav-link {{ Request::is('contenido/' . $category['id'] . '/0' ) ? 'active' : '' }}"
-               href="{{ route('content', [$category['id'], 0]) }}">
-                {{ $category['nombre'] }}
-            </a>
+            @if(isset($category['url']) && !empty($category['url']))
+                <a class="nav-link {{ Request::is('contenido/' . $category['id'] . '/0' ) ? 'active' : '' }}"
+                   href="{{ $category['url'] }}">
+                    {{ $category['nombre'] }}
+                </a>
+            @else
+                <a class="nav-link {{ Request::is('contenido/' . $category['id'] . '/0' ) ? 'active' : '' }}"
+                   href="{{ route('content', [$category['id'], 0]) }}">
+                    {{ $category['nombre'] }}
+                </a>
+            @endif
         @else
             <a class="nav-link dropdown-toggle" href="https://bootstrapthemes.co" id="categories_multimenu"
                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -17,10 +24,18 @@
             <ul class="dropdown-menu" aria-labelledby="categories_multimenu">
                 @foreach($category['subcategories'] as $subcategory)
                     <li class="nav-item">
-                        <a class="dropdown-item {{ Request::is('contenido/' . $category['id'] . '/' . $subcategory['id']) ? 'active' : '' }}"
-                           href="{{ route('content', [$category['id'], $subcategory['id']]) }}">
-                            {{ $subcategory['nombre'] }}
-                        </a>
+                        @if(isset($subcategory['url']) && !empty($subcategory['url']))
+                            <a class="dropdown-item {{ Request::is('contenido/' . $category['id'] . '/' . $subcategory['id']) ? 'active' : '' }}"
+                               href="{{ $subcategory['url'] }}">
+                                {{ $subcategory['nombre'] }}
+                            </a>
+                        @else
+                            <a class="dropdown-item {{ Request::is('contenido/' . $category['id'] . '/' . $subcategory['id']) ? 'active' : '' }}"
+                               href="{{ route('content', [$category['id'], $subcategory['id']]) }}">
+                                {{ $subcategory['nombre'] }}
+                            </a>
+                        @endif
+
                     </li>
                 @endforeach
             </ul>
