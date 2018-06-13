151dc56cfdbf67e8239e862694e3404dfe27204e
Jose Sicajan <sicajancoy@gmail.com>
2018-06-13 05:40:17 -0600

    card left without background

--- resources/views/elements/section/card-left.blade.php
+++ resources/views/elements/section/card-left.blade.php
@@ -1,5 +1,5 @@
 <div class="row flex-column align-items-end mb-28px">
-    <button data-href="{{ $link }}" class="ajax_link card-primary bg-grid-secondary col-12 d-flex flex-row img-cover" style="background-image: url({{ $background_url }});">
+    <button data-href="{{ $link }}" class="ajax_link card-primary col-12 d-flex flex-row img-cover" style="background-image: url({{ $background_url }});">
         <div class="d-flex flex-column justify-content-center col-6 h-100"></div>
     </button>
 </div>
