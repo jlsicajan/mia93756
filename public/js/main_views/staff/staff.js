9241127a997e73bf1b0d3b8bdbbfb1154f9824e7
Jose Sicajan <sicajancoy@gmail.com>
2018-07-06 02:59:11 -0600

    staff second image

--- public/js/main_views/staff/staff.js
+++ public/js/main_views/staff/staff.js
@@ -4,15 +4,23 @@ $('.keep_reading').unbind('click').click(function () {
     console.warn(staff_selected);
     // let user_selected = users_blog.find(us => us.id == staff_selected.usuario_id);
 
-
     console.log(users_blog);
-    $('#staff_modal .modal-body').empty().html(staff_selected['texto']);
     $('#staff_modal .modal-title').empty().html(staff_selected['locutor']);
+
+    $('#staff_modal .modal-body').empty().html(staff_selected['texto']);
+
+    let container = $('<div>').addClass('row staff_description p-3');
+
     let staff_image = $('<div>').addClass('img-cover img-clean-display').css('background-image', 'url(' + url_source + '/uploads/staff/' + staff_selected['imagen'] + ')');
-    $('#staff_modal .modal-body').append(staff_image);
+    let second_staff_image = $('<div>').addClass('img-cover img-clean-display').css('background-image', 'url(' + url_source + '/uploads/staff/' + staff_selected['imagen_dos'] + ')');
+
+    container.append([$('<div>').addClass('col-12 col-md-12 mb-2 p-3').append(staff_image), $('<div>').addClass('col-12 col-md-12 mb-2 p-3').append(second_staff_image)]);
+    $('#staff_modal .modal-body').append(container);
 
     console.log(staff_selected['texto']);
 });
+//# sourceMappingURL=staff.js.map
+
 //# sourceMappingURL=staff.js.map
 
 //# sourceMappingURL=staff.js.map
