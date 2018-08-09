1bb2aef07a5632176c45ea58b029bd48be7c5cbf
Jose Sicajan <sicajancoy@gmail.com>
2018-08-08 22:11:24 -0600

    mayus

--- resources/views/main_views/the20/index.blade.php
+++ resources/views/main_views/the20/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
-@section('title', 'mia 93.7 la vida es mejor cantando')
-@section('description', 'Radio mia 93.7 la vida es mejor cantando')
+@section('title', 'mia 93.7 La vida es mejor cantando')
+@section('description', 'Radio mia 93.7 La vida es mejor cantando')
 @section('og_image', env('URL_RADIO_INFO_PATH') . \App\Radio::get_logo())
 
 @section('content')
