95f09cde55bcc3002306b8987f417c58c99f4056
Jose Sicajan <sicajancoy@gmail.com>
2018-09-18 17:42:08 -0600

    Common form file with responsive fix

--- resources/views/main_views/forms/index.blade.php
+++ resources/views/main_views/forms/index.blade.php
@@ -42,47 +42,14 @@
             </div>
 
             <div class="row justify-content-center">
-                {{-- Formulario --}}
-                <form class="col-sm-12 col-md-8 col-lg-6 form-mia-back" method="POST" data-href="/" action="/formulario/{{$category}}" accept-charset="UTF-8" enctype="multipart/form-data">
-                    <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                    <input type="hidden" name="_token" value="{{ $category }}">
-                    <div class="form-group">
-                        <label for="nombre_completo">Nombre Completo</label>
-                        <input type="text" class="form-control" name="nombre_completo" id="nombre_completo" placeholder="Nombre completo..." required>
-                    </div>
-                    <div class="form-group">
-                        <label for="nombre_secundario">Nombre de la pareja</label>
-                        <input type="text" class="form-control" name="nombre_secundario" id="nombre_secundario" placeholder="Nombre completo de la pareja..." required>
-                    </div>
-                    <div class="form-group">
-                        <label for="telefono">No. de Teléfono</label>
-                        <input type="text" class="form-control" name="telefono" id="telefono" placeholder="0000-0000" required>
-                    </div>
-                    <div class="form-group">
-                        <label for="email">Correo Electrónico</label>
-                        <input type="email" class="form-control" name="email" id="email" placeholder="nombre@ejemplo.com" required>
-                    </div>
-                    <div class="form-group">
-                        <label for="historia">Cuéntanos tu historia: </label>
-                        <textarea type="text" rows="5" class="form-control" name="historia" id="historia" required></textarea>
-                    </div>
-                    <div class="form-group">
-                        <label for="foto">Foto</label>
-                        <input type="file" class="form-control" name="foto" id="foto">
-                    </div>
-
-                    <button type="submit" class="btn btn-secondary">Enviar</button>
-                    {{-- <button class="ajax_link nav-link {{ Request::is('contenido/' . $category . '/0' ) ? 'active' : '' }}"
-                            data-href="/">
-                        {{ $category }}
-                    </button> --}}
-                </form>
+                {{-- Form --}}
+                @include('main_views.forms.common_form')
             </div>
         </div>
     </div>
     @include('elements.radio.live_radio_element')
     <style type="text/css">
-        .bg-grid-secondary{
+        .bg-grid-secondary {
             background: linear-gradient(to bottom right, #bd3188, #e2008c) !important;
         }
     </style>
--- resources/views/main_views_content/forms/index.blade.php
+++ resources/views/main_views_content/forms/index.blade.php
@@ -33,38 +33,9 @@
         @endif
     </div>
 
-    <div class="row justify-content-center">
-        {{-- Formulario --}}
-        <form class="col-sm-12 col-md-8 col-lg-6 form-mia-back" method="POST" action="/formulario/{{$category}}" accept-charset="UTF-8" enctype="multipart/form-data">
-            <input type="hidden" name="_token" value="{{ csrf_token() }}">
-            <input type="hidden" name="_token" value="{{ $category }}">
-            <div class="form-group">
-                <label for="nombre_completo">Nombre Completo</label>
-                <input type="text" class="form-control" name="nombre_completo" id="nombre_completo" placeholder="Nombre completo..." required>
-            </div>
-            <div class="form-group">
-                <label for="nombre_secundario">Nombre de la pareja</label>
-                <input type="text" class="form-control" name="nombre_secundario" id="nombre_secundario" placeholder="Nombre completo de la pareja..." required>
-            </div>
-            <div class="form-group">
-                <label for="telefono">No. de Teléfono</label>
-                <input type="text" class="form-control" name="telefono" id="telefono" placeholder="0000-0000" required>
-            </div>
-            <div class="form-group">
-                <label for="email">Correo Electrónico</label>
-                <input type="email" class="form-control" name="email" id="email" placeholder="nombre@ejemplo.com" required>
-            </div>
-            <div class="form-group">
-                <label for="historia">Cuéntanos tu historia: </label>
-                <textarea type="text" rows="5" class="form-control" name="historia" id="historia"></textarea>
-            </div>
-            <div class="form-group">
-                <label for="foto">Foto</label>
-                <input type="file" class="form-control" name="foto" id="foto">
-            </div>
-
-            <button type="submit" class="btn btn-secondary">Enviar</button>
-        </form>
+    <div class="row">
+        {{-- Form --}}
+        @include('main_views.forms.common_form')
     </div>
 </div>
 <style type="text/css">
