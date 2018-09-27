767ea4cffabe00f8063935c3d202f277b8d6f96a
Jose Sicajan <sicajancoy@gmail.com>
2018-09-27 04:31:27 -0600

    Weeding form updated

--- resources/views/main_views/forms/common_form.blade.php
+++ resources/views/main_views/forms/common_form.blade.php
@@ -1,34 +1,71 @@
-<div class="col-11 col-md-7">
-    <form class="form-mia-back">
+<div class="col-11 col-md-7 bg-white p-5">
+    <form class="wedding_form">
         <input type="hidden" name="_token" value="{{ csrf_token() }}">
-        <input type="hidden" name="_token" value="{{ $category }}">
+        <input type="hidden" name="category" value="{{ $category }}">
+
         <div class="form-group">
-            <label for="nombre_completo">Nombre Completo</label>
-            <input type="text" class="form-control" name="nombre_completo" id="nombre_completo"
-                   placeholder="Nombre completo..." required>
-        </div>
-        <div class="form-group">
-            <label for="nombre_secundario">Nombre de la pareja</label>
-            <input type="text" class="form-control" name="nombre_secundario" id="nombre_secundario"
-                   placeholder="Nombre completo de la pareja..." required>
-        </div>
-        <div class="form-group">
-            <label for="telefono">No. de Teléfono</label>
-            <input type="text" class="form-control" name="telefono" id="telefono" placeholder="0000-0000" required>
-        </div>
-        <div class="form-group">
-            <label for="email">Correo Electrónico</label>
-            <input type="email" class="form-control" name="email" id="email" placeholder="nombre@ejemplo.com" required>
-        </div>
-        <div class="form-group">
-            <label for="historia">Cuéntanos tu historia: </label>
-            <textarea type="text" rows="5" class="form-control" name="historia" id="historia" required></textarea>
-        </div>
-        <div class="form-group">
-            <label for="foto">Foto</label>
-            <input type="file" class="form-control" name="foto" id="foto">
+            <input type="text" class="form-control input_bordered_primary" name="name" placeholder="Nombre completo" required="required">
         </div>
 
-        <button type="submit" class="btn btn-secondary">Enviar</button>
+        <div class="form-group">
+            <input type="text" class="form-control input_bordered_primary" name="couple_name" placeholder="Nombre completo de la pareja" required="required">
+        </div>
+
+        <div class="form-group">
+            <input type="text" class="form-control input_bordered_primary" name="phone" placeholder="Número de teléfono" required="required">
+        </div>
+
+        <div class="form-group">
+            <input type="email" class="form-control input_bordered_primary" name="email" placeholder="nombre@ejemplo.com" required="required">
+        </div>
+
+        <div class="form-group">
+            <textarea type="text" rows="7" class="form-control input_bordered_primary" name="history" required="required" placeholder="Cuéntanos tu historia"></textarea>
+        </div>
+
+        <div class="form-group">
+            <input type="file" class="form-control input_bordered_primary" name="foto">
+        </div>
+
+        <div class="form-group text-center">
+            <button type="button" class="btn btn-secondary wedding_form_send">Enviar</button>
+        </div>
+
     </form>
 </div>
+
+<style type="text/css">
+    /*this should be moved to scss and compiled with gulp (pending, also move rootcode css to scss) */
+    .input_bordered_primary {
+        display: inline-block;
+        width: 100%;
+        margin: 13px 0;
+        padding: 10px;
+        border: 2px dashed #ff009e;
+        outline: none;
+        font-size: 20px;
+        font-family: 'Economica', 'Arial', sans-serif;
+        font-weight: 400;
+        transition: all 0.2s ease;
+    }
+
+    .input_bordered_primary:focus {
+        border-color: #b4006c;
+    }
+
+    .wedding_form_send {
+        width: 150px;
+        height: 50px;
+        padding: 10px 15px;
+        margin-top: 20px;
+        border: none;
+        outline: none;
+        cursor: pointer;
+        color: white;
+        font-family: 'Economica', 'Arial', sans-serif;
+        font-size: 20px;
+        font-weight: 700;
+        background: #ff009e;
+        transition: all 0.2s ease;
+    }
+</style>
